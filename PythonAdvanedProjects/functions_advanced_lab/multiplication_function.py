c8035191bf9bb11aa0923a128f756c13177956ce
Gloriya <gloriya1995@gmail.com>
2023-06-12 14:31:57 +0300

    Advanced Project

---
+++ PythonAdvanedProjects/functions_advanced_lab/multiplication_function.py
@@ -0,0 +1,9 @@
+def multiply(*args):
+    res = 1
+    for char in args:
+        res *= char
+    return res
+
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))
